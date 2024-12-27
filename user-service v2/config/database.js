53659725d6d423b131e3dc3d5a6e82bcb99c300c
Mohamed Magdy <137237146+momagdyy@users.noreply.github.com>
2024-12-27 02:50:29 +0200

    Add files via upload

---
+++ user-service v2/config/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+});
+
+sequelize
+    .authenticate()
+    .then(() => console.log("Database connected successfully"))
+    .catch((err) => console.error("Unable to connect to the database:", err));
+
+module.exports = sequelize;
